e403f77f5ca12f76d10eab84b4ce03d6209c4525
Ratta38 <k024g1030@g.neec.ac.jp>
2025-09-29 12:43:43 +0900

    01_01_確認課題

--- 01-01/main.cpp
+++ 01-01/main.cpp
@@ -1,9 +1,55 @@
-#include <stdio.h>
 #include <list>
+#include <stdio.h>
+#include <iostream>
 
-int main()
-{ 
-	std::list<char[]> list("東京", "神田", "秋葉原", "御徒町", "上野", "鶯谷", "日暮里", "西日暮里", "田端");
+/// <summary>
+/// 駅のリストを出力する関数
+/// </summary>
+/// <param name="stations">駅名のリスト</param>
+void PrintStations(const char* number, const std::list<const char*>& stations) {
+	// 年数
+	std::cout << number << "\n";
 
-	return 0; 
+	for (auto station : stations) {
+		std::cout << station << "\n";
+	}
+
+	// 改行
+	std::cout << "\n";
+}
+
+/// <summary>
+/// 指定した駅の前に新しい駅を追加
+/// </summary>
+/// <param name="stations">駅名のリスト</param>
+/// <param name="target">挿入する位置</param>
+/// <param name="newStation">追加する駅名</param>
+void InsertBefore(std::list<const char*>& stations, const char* target, const char* newStation)
+{
+	for (auto itr = stations.begin(); itr != stations.end(); ++itr) {
+		if (std::strcmp(*itr, target) == 0) {
+			stations.insert(itr, newStation); // 西日暮里の追加
+			return;
+		}
+	}
+}
+
+int main() {
+	// 山手線駅名リスト
+	std::list<const char*> stations = {"Tokyo",  "Kanda",     "Akihabara",        "Okachimachi",  "Ueno",         "Uguisudani", "Nippori", "Tabata",  "Komagome", "Sugamo",
+	                               "Otsuka", "Ikebukuro", "Mejiro",           "Takadanobaba", "Shin-Okubo",   "Shinjuku",   "Yoyogi",   "Harajuku",      "Shibuya", "Ebisu",    "Gotanda",
+	                               "Osaki",  "Shinagawa", "Tamachi",      "Hamamatsucho", "Shimbashi",  "Yurakucho"};
+
+	// 1970年
+	PrintStations("1920年", stations);
+
+	// 2019年
+	InsertBefore(stations, "Tabata", "Nishi-Nippori");
+	PrintStations("2019年", stations);
+
+	// 2022年
+	InsertBefore(stations, "Tamachi", "Takanawa Gateway");
+	PrintStations("2022年", stations);
+
+	return 0;
 }
