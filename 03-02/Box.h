d3a00b9cf27c821a5a77d6ff7afac6c0089a0a9e
Ratta38 <k024g1030@g.neec.ac.jp>
2025-12-26 16:59:44 +0900

    03_02_確認課題

--- 03-02/Box.h
+++ 03-02/Box.h
@@ -1,12 +1,18 @@
 #pragma once
+#include <algorithm>
+#include <type_traits>
 
-//template<typename T1, typename T2> class Box {
-//public:
-//	T1 num1_;
-//	T2 num2_;
-//
-//	Box(T1 num1, T2 num2) : num1_(num1), num2_(num2) {}
-//
-//	// 二つの値を比較して小さい値を返す
-//	auto Min() -> decltype(std::min(num1_ + num2_)) { return std::min(num1_, num2_); }
-//};
+template<typename T1, typename T2> 
+class Box {
+public:
+	T1 num1_;
+	T2 num2_;
+
+	Box(T1 num1, T2 num2) : num1_(num1), num2_(num2) {}
+
+	// 二つの値を比較して小さい値を返す
+	auto Min() {
+		using Common = std::common_type_t<T1, T2>;
+		return std::min(static_cast<Common>(num1_), static_cast<Common>(num2_));
+	}
+};
--- 03-02/main.cpp
+++ 03-02/main.cpp
@@ -1,20 +1,21 @@
+#include "Box.h"
 #include <iostream>
-//#include "Box.h"
 #include <string>
 
 int main() {
-	/*Box<int, int> b1(10, 20);
+	Box<int, int> b1(10, 20);
 	Box<int, float> b2(12, 5.0f);
 	Box<int, double> b3(1, 2.34);
 	Box<float, float> b4(40.0f, 39.0f);
 	Box<float, double> b5(10.0f, 9.0);
 	Box<double, double> b6(10.0, 20.0);
 
-	std::cout << std::to_string(b1.Min()) << std::endl;
-	std::cout << std::to_string(b2.Min()) << std::endl;
-	std::cout << std::to_string(b3.Min()) << std::endl;
-	std::cout << std::to_string(b4.Min()) << std::endl;
-	std::cout << std::to_string(b5.Min()) << std::endl;*/
+	std::cout << b1.Min() << std::endl;
+	std::cout << b2.Min() << std::endl;
+	std::cout << b3.Min() << std::endl;
+	std::cout << b4.Min() << std::endl;
+	std::cout << b5.Min() << std::endl;
+	std::cout << b6.Min() << std::endl;
 
 	return 0;
 }
